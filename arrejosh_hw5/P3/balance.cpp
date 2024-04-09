8b783b7890adc8f2fe2f3a2c1887a09d2b0c96ff
2004joshua <arrejosh@bu.edu>
2024-04-09 18:36:39 -0400

    homework 5 folder

---
+++ arrejosh_hw5/P3/balance.cpp
@@ -0,0 +1,12 @@
+#include "node.h"
+
+// your includes here
+
+bool isWeightBalanced(node* root, int k) { 
+
+	// your implementation here
+
+    return 0; // don't forget to upate this
+}
+
+// your helper methods here
---
+++ arrejosh_hw5/nextklargest/nextKLargest.cpp
@@ -0,0 +1,6 @@
+#include "BST.h"
+
+/* this is the function that you need to implement */
+int BST::nextKLargest(int x, int k) {
+    return 0; // don't forget to change the return value
+}
